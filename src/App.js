ed3e128d952828681b5dc82dae6916831d4e1070
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-01-28 13:13:10 +0000

    adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 className='display-1'>
+        DOG
+        <small className="text-muted"> app</small>
+      </h3>
+      <div className="card" style={{ width: '18rem' }} >
+        <img src="https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0d3f33fb6aa6e0154b7713a00454c83d" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card with stretched link</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary stretched-link">Go somewhere</a>
+        </div>
+      </div>
     </div>
   );
 }
